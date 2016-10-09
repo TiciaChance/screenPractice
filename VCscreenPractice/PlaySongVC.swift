b5893ea339494d6193fb7e62bd4ee5eb2924b896
TiciaChance <laticia.chance@flatironschool.com>
2016-10-09 10:52:57 -0400

    set up for third screen

--- VCscreenPractice/PlaySongVC.swift
+++ VCscreenPractice/PlaySongVC.swift
@@ -1,5 +1,5 @@
 //
-//  MusicListVC.swift
+//  PlaySongVC.swift
 //  VCscreenPractice
 //
 //  Created by Laticia Chance on 10/9/16.
@@ -8,27 +8,15 @@
 
 import UIKit
 
-class MusicListVC: UIViewController {
+class PlaySongVC: UIViewController {
 
+    @IBOutlet weak var songTitleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        let bgcolor = UIColor(red: 87/255, green: 133/255, blue: 138/255, alpha: 1.0)
-        view.backgroundColor = bgcolor
-        
 
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func backButtonTapped(_ sender: AnyObject) {
-        dismiss(animated: true, completion: nil)
-    }
-  
-    @IBAction func rdScreenTapped(_ sender: AnyObject) {
-    }
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
