3a6fa27669e253a58316ed2e7a7f84e659093a91
TiciaChance <laticia.chance@flatironschool.com>
2016-10-09 10:25:29 -0400

    basic update

--- VCscreenPractice/MusicListVC.swift
+++ VCscreenPractice/MusicListVC.swift
@@ -12,10 +12,17 @@ class MusicListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        let bgcolor = UIColor(red: 87/255, green: 133/255, blue: 138/255, alpha: 1.0)
+        view.backgroundColor = bgcolor
+        
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func backButtonTapped(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
